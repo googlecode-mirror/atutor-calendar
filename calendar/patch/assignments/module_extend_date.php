0b23968c33c5bf471e6f22cd845c5eb42137eb00
heratgandhi <heratgandhi@gmail.com>
2012-06-13 03:08:48 +0000

    focus on select and patch changed

--- calendar/patch/assignments/module_extend_date.php
+++ calendar/patch/assignments/module_extend_date.php
@@ -36,17 +36,19 @@ function assignments_extend_date($course_id=null){
     if($row_count > 0){
     $index=0;
         while($row = mysql_fetch_assoc($result)){
-			if(  strpos( $row['date_due']."", '0000-00-00' ) !== false ||
+			/*if(  strpos( $row['date_due']."", '0000-00-00' ) !== false ||
 				strpos( $row['date_cutoff']."", '0000-00-00' ) !== false )
 			{
 				continue;
 			}
 			else
-			{
+			{*/
 				$assignment_id = $row['assignment_id'];
 				$unix_ts = strtotime($row['date_due']);
 				$time = date("h:i A",$unix_ts);
-				$assignments[$index] =  array(
+                if( strpos( $row['date_due']."", '0000-00-00' ) === false )
+                {
+					$assignments[$index] =  array(
 									"id"=>rand(5000,9000)."",
 									"title"=> "Due date of ".$row['title'],
 									"start"=>$row['date_due'],
@@ -57,10 +59,13 @@ function assignments_extend_date($course_id=null){
 									"editable"=>false
 									) ;
 								 
-				$unix_ts = strtotime($row['date_cutoff']);                  
-				$time = date("h:i A",$unix_ts);
-				$index++;                               
-				$assignments[$index] =  array(
+					$unix_ts = strtotime($row['date_cutoff']);                  
+					$time = date("h:i A",$unix_ts);
+					$index++;
+                }
+				if( strpos($row['date_cutoff']."", '0000-00-00' ) === false )
+                {
+                $assignments[$index] =  array(
 								"id"=>rand(5000,9000)."",
 								"title"=>"Cut off date of ".$row['title'],
 								"start"=>$row['date_cutoff'],
@@ -70,10 +75,10 @@ function assignments_extend_date($course_id=null){
 								"textColor" => "black",
 								"editable"=>false 
 								 ) ;            
-			$index++;
+                $index++;
+                }
 			}
-        }
-    }    
+        }    
     return $assignments;
 }
 ?>
